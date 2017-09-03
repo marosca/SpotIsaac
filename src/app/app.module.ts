9b13d7e6e95da78db34b206352c5bc1cf00258bf
Mariola Moreno <mariolamoreno@hotmail.es>
2017-09-03 15:13:53 +0200

    Subida

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
+//rutas
+import { APP_ROUTING } from './app.routes';
+
+//pipes
+import { SinfotoPipe } from './pipes/sinfoto.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+
+//servicios
+import { SpotifyService } from './services/spotify.service';
+
+//componentes
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { ArtistaComponent } from './components/artista/artista.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SearchComponent,
+    NavbarComponent,
+    ArtistaComponent,
+	SinfotoPipe,
+	DomseguroPipe
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING,
+    HttpModule,
+	  FormsModule
   ],
-  providers: [],
+  providers: [
+    SpotifyService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
