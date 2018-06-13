b6e2a9d682569bbdf0a0b01917a78f3e9c2dd85c
Mariola Moreno <mariolamoreno@hotmail.es>
2018-06-13 16:39:01 +0200

    eliminar todo

--- src/app/pipes/sinfoto.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SinfotoPipe } from './sinfoto.pipe';
-
-describe('SinfotoPipe', () => {
-  it('create an instance', () => {
-    const pipe = new SinfotoPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
